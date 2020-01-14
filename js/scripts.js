abdeeffd83d0273f483498bbcf1d6ef006aeb57d
Antonio Cruz and Johnny Duverseau <johnny@>
2020-01-14 11:04:51 -0800

    add lion and css/js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").fadeToggle();
-    $("#walrus-hidden").fadeToggle();
-    
-    /*$("#walrus-hidden").slideUp();*/
-    
+    $("#walrus-hidden").fadeToggle();    
+  });
+});
+$(document).ready(function() {
+  $(".touch").click(function() {
+    $("#lion-showing").fadeToggle();
+    $("#lion-hidden").fadeToggle();        
   });
 });
